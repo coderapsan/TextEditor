af15e0efe70763fd105f2da5a0e8ba73c0c25930
coderapsan <52061199+coderapsan@users.noreply.github.com>
2023-07-14 00:53:02 +0545

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  
+
+  return (
+    <div>
+      <Content />
+     
+     
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
